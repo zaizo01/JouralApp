083877d7b458e11f9e4ac2ae8ed80d42ac7ec839
zaizo01 <e.zaizortega@gamil.com>
2022-08-09 07:00:08 -0400

    chnages

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./styles/styles.scss";
+import store from "./store";
 
 // Install bootstrap
 // npm install bootstrap
 // npm i -D sass-loader@10 sass
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(store).use(router).mount("#app");
