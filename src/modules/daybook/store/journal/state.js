5e17c5a330126972a6e98faf630ce300be4f6875
zaizo01 <e.zaizortega@gamil.com>
2022-08-10 07:13:43 -0400

    changes

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,4 +1,11 @@
 export default () => ({
   isLoading: true,
-  entries: [1, 2, 3],
+  entries: [
+    {
+      id: new Date().getTime(),
+      date: new Date(),
+      picture: null,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus corporis incidunt corrupti, ut aut sunt accusamus, dolorum voluptas ratione iure reiciendis ducimus? Tenetur eligendi accusantium eius, corporis tempora eos nisi.",
+    },
+  ],
 });
