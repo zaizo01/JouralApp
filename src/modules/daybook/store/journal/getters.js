f07ef1723a31a2618793de719cee3484daa08ee1
zaizo01 <e.zaizortega@gamil.com>
2022-08-09 23:56:40 -0400

    vuex config and section definition done

--- src/modules/daybook/store/journal/getters.js
+++ src/modules/daybook/store/journal/getters.js
@@ -1,3 +1,7 @@
 // export const myGetter = (state) => {
 // return state
 // }
+
+export const getEntriesByTerm = (/*state*/) => {};
+
+export const getEntryById = (/*state*/) => {};
--- src/modules/daybook/store/journal/mutations.js
+++ src/modules/daybook/store/journal/mutations.js
@@ -1,3 +1,9 @@
 // export const myMutation = (state) => {
 
 // }
+
+export const setEntries = (/*state*/) => {};
+
+export const updateEntry = (/*state*/) => {};
+
+export const addEntry = (/*state*/) => {};
